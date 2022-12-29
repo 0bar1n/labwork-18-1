e7801694e4bd34c7e677b70d128eadd626dd4212
barinskaya <1388zxc@gmail.com>
2022-12-29 10:26:45 +0300

    Добавьте файлы проекта.

---
+++ labwork 18-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace labwork_18_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k; //переменная целого типа
+            double s = 0;//переменная вещественного типа
+            double fact = 1; //переменная вещественного типа
+            Console.WriteLine("Введите количество элементов последовательности"); //вывод на консоль
+            k = Convert.ToInt32(Console.ReadLine());//ввод и задание переменной
+            for (int i = 1; i <= k; i++) //цикл, пока выполняется условие
+            {
+                fact = fact * i; //факториал
+                s += 1 / fact; //сумма n элементов последовательности
+            }
+            Console.WriteLine(s); //вывод результата
+            Console.ReadLine(); //программа ожидает нажатия клавиши для завершения работы
+        }
+    }
+}
